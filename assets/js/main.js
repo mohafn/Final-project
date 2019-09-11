2044c3af55f5d57822123c8928d4539991cc0d3d
mohafn <52421083+mohafn@users.noreply.github.com>
2019-09-11 16:25:33 +0300

    final

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+AOS.init({
+    duration: 1500
+});
+
